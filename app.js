c758cac6675b36b9c89c8e00dec3fe69868b0bc9
winniekwoknorth <winniekwokuk@yahoo.com>
2023-11-20 14:59:03 +0000

    change response to comment

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const {getTopics}= require('./controllers/topics.controllers.js')
 const { handleCustomErrors, handlePsqlErrors, handleServerErrors } = require('./err.js');
 
-app.use(express.json());
 app.get('/api/topics', getTopics)
 app.use(handleCustomErrors)
 app.use(handlePsqlErrors)
