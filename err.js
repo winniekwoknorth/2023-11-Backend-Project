1fbee0ee6fe78ebce0e0f14408dadc8ed8e2e5dc
winniekwoknorth <winniekwokuk@yahoo.com>
2023-11-21 15:17:43 +0000

    complete task 4

--- err.js
+++ err.js
@@ -6,13 +6,11 @@ exports.handleCustomErrors = ((err, req, res, next) => {
     else next(err)
 })
 exports.handlePsqlErrors = ((err, req, res, next) => {
-    console.log(err)
     if(err.code === '23502' || '22P02'){
         res.status(400).send({msg: "Bad request"})
     }
     else next(err)
 })
 exports.handleServerErrors = ((err, req, res, next) => {
-    console.log(err)
     res.status(500).send({msg: "internal server error"})
     })
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,9 +1,15 @@
 const db = require('../db/connection.js')
-exports.selectTopics= () => {
-    const queryString = `SELECT * FROM topics`
-    return db.query(queryString)
-        .then((result) => {
-            return result.rows;
+exports.selectArticles= (article_id) => {
+    const queryString = `SELECT * FROM articles WHERE article_id=$1`
+    const queryArray = [article_id]
+    return db.query(queryString,queryArray)
+    .then((result) => {
+        if (result.rows.length === 0) {
+            return Promise.reject({
+                status: 404,
+                msg: 'article does not exist' })
+        }
+            return result.rows[0];
         })
 }
 
